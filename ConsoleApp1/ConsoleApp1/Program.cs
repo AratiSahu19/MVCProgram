7b31e261aaa7df792e1ffe838f3dda5fd1165d10
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-26 16:02:48 +0530

    committed

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int rem = 0;
+            int rev = 0;
+            Console.WriteLine("enter a number");
+            num = int.Parse(Console.ReadLine());
+            int cpynum = num;
+            while(num!=0)
+            {
+                rem = num % 10;
+                num = num / 10;
+                rev = rev + (rem * rem * rem);
+
+            }
+            if(cpynum==rev)
+            { 
+                Console.WriteLine("no isarmstrong");
+            }
+            else
+            {
+                Console.WriteLine("not armstrong no");
+            }
+        }
+
+    }
+}
