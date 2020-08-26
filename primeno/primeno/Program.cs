4b99a9c8e3b355ba859f56d2c47909954f36f445
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-26 16:39:57 +0530

    prime no program

---
+++ primeno/primeno/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Dynamic;
+
+namespace primeno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, i = 2, k = 1;
+            Console.WriteLine("enter number");
+            num = int.Parse(Console.ReadLine());
+            while (i <= num)
+            {
+                if (num % i == 0)
+                {
+                    k = k + 1;
+
+                }
+                i++;
+            }
+            if(k==2)
+            {
+                Console.WriteLine("prime no");
+
+            }
+            else
+            {
+                Console.WriteLine("not prime");
+            }
+            Console.ReadKey();
+        }
+    }
+}
