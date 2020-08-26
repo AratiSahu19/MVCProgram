526accc5072af38532ac7c774b5d50ae988b54b5
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-26 16:52:55 +0530

    string example in console

---
+++ stringprintcsharp/stringprintcsharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace stringprintcsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine("enter your name");
+            string name = Console.ReadLine();
+           // Console.WriteLine("My name is : {0}", name);// by using placeholder to display the name with message
+            Console.WriteLine("My name is :" + name);// by using concatenation
+            Console.ReadKey();
+        }
+    }
+}
