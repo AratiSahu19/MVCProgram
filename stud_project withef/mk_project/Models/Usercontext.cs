4b55877469b4c99756a42d52437ec9c69cf005c5
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-26 15:30:09 +0530

    mvc crud withef

---
+++ stud_project withef/mk_project/Models/Usercontext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace mk_project.Models
+{
+    public class Usercontext:DbContext
+    {
+        public DbSet <UserModel> UsersModels { get; set; }
+    }
+}
