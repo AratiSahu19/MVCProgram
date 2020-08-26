e126aa331c7d6a0192ff2b49bd8fe8c2b6ff06e0
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-26 15:11:40 +0530

    example of boxing and unboxing

---
+++ boxing_unboxing/boxing_unboxing/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace boxing_unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num = 123;
+            //object obj = num;
+            //Console.WriteLine(num);
+            //Console.WriteLine(obj);
+            object obj = 123;
+            int num = (int)obj;
+            Console.WriteLine(obj);
+            Console.WriteLine(num);
+    }
+    }
+}
