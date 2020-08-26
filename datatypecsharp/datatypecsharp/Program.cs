dd34f144c7683e1c116b873e1f25e0531348a052
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-26 16:06:54 +0530

    code tofind the range of datatype

---
+++ datatypecsharp/datatypecsharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace datatypecsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //find the range of byte datatype
+
+            Console.WriteLine(byte.MinValue);
+            Console.WriteLine(byte.MaxValue);
+
+            //range of int datatype
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(int.MaxValue);
+
+        }
+    }
+}
