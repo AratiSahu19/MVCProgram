c11063009bcdcbc639831a3b8d21f613c4e8618f
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-26 15:13:22 +0530

    example of readonly and constant

---
+++ readonly_const/readonly_const/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace readonly_const
+{
+    
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Test obj = new Test();
+                obj.Check();
+                Console.ReadLine();
+            }
+        }
+        class Test
+        {
+            readonly int read=10 ;
+            const int cons =10;
+      
+        public Test()
+        {
+            read = 100;
+
+        }
+        public void Check()
+            {
+           
+                Console.WriteLine("Read only : {0}", read);
+                Console.WriteLine("const : {0}", cons);
+            }
+        }
+}
