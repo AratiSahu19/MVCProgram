c5e1c5464e433658373a4829e53d264ea17ba227
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-26 16:16:56 +0530

    grid view crud operation

---
+++ gridview/gridview/WebForm2.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="gridview.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False">
+                <Columns>
+                    <asp:BoundField DataField="id" HeaderText="id" />
+                    <asp:BoundField DataField="sname" HeaderText="name" />
+                    <asp:BoundField DataField="marks" HeaderText="marks" />
+                    <asp:BoundField DataField="subject" HeaderText="subject" />
+                    <asp:BoundField DataField="phonenumber" HeaderText="mobileno" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ gridview/gridview/WebForm2.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace gridview
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-RIJR5L2\\sqlexpress;integrated security=true;database=practicedb");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+            if (!IsPostBack)
+            {
+                datashow();
+            }
+        }
+            public void  datashow()
+            {
+            SqlDataAdapter da = new SqlDataAdapter("select * from students", "Data Source = DESKTOP - RIJR5L2\\sqlexpress; integrated security = true; database=practicedb");
+            DataTable dt = new DataTable();
+           // DataSet dt = new DataSet();
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            }
+
+        }
+    }
+
