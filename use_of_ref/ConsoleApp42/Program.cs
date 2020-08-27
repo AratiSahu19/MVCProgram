3745ba754187234ec2e40b49ebe3bc49169f6ed6
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-27 12:38:59 +0530

    use of ref keyword

---
+++ use_of_ref/ConsoleApp42/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp42
+{
+    class Program
+    {
+         static void show( ref int a, ref  int b )
+        {
+            
+            int x = a;
+            int y = b; 
+            Console.WriteLine("value of a ="+x );
+            Console.WriteLine("value of b=" + y);
+
+        }
+        static void Main(string[] args)
+        {
+            int a = 6,b=9;
+
+            show( ref a,ref b );
+            Console.ReadKey();
+        }
+    }
+}
