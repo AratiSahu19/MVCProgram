5cbb8defef718168540bca3f70d766670a5bd9be
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-27 12:37:30 +0530

    use of params

---
+++ use_of_params/ConsoleApp45/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp45
+{
+    class Program
+    {
+        static long add(params long[] v)
+        {
+            long  t, i;
+            for (i=0,t=0;i<v.Length;i++)
+            t += v[i];
+            Console.WriteLine(" number is" + t);
+
+            return t;
+
+        }
+        static void Main(string[] args)
+        {
+            long x = add(45, 74, 23);
+
+            Console.ReadKey();
+        }
+    }
+}
