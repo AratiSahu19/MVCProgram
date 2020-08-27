3b281964a5fd19c445b6e688df3275e31ecb015e
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-28 01:24:28 +0530

    if else exmp

---
+++ ifelseexmp/ConsoleApp5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age ;
+            int n = 18;
+            Console.WriteLine("enter the age :");
+            int a = Convert.ToInt32(Console.ReadLine());
+           
+            if (a >= 18)
+                Console.WriteLine("the person can vote " + a);
+            else
+                Console.WriteLine(" not eligible to vote " + a);
+            Console.ReadKey();
+        }
+    }
+}
