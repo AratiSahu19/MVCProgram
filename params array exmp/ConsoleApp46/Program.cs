4b4df774029bc95ed306211ada69bfdf0a2dd20c
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-27 11:46:39 +0530

    params exmp

---
+++ params array exmp/ConsoleApp46/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp46
+{
+    class Program
+    {
+         static int add(params int[] val)
+        {
+            int sum = 0, i;
+            for( i=1;i<val.Length;i++)
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine(" sum is " + sum);
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter numbers");
+
+
+            int[] a = new int[5];
+            add(a);
+           
+        }
+    }
+}
