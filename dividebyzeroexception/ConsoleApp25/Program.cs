27f3d50d4e5d2dd3faad144289e175abc51a6fe2
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-28 01:02:40 +0530

    divide by zero exception

---
+++ dividebyzeroexception/ConsoleApp25/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int  r;
+                Console.WriteLine("enter two nos");
+                int n1 = Convert.ToInt32(Console.ReadLine());
+                int n2 = Convert.ToInt32(Console.ReadLine());
+                r = n1 / n2;
+                Console.WriteLine("number is " + r);
+
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine(" not divisible");
+
+            }
+            catch (Exception ee)
+            {
+                Console.WriteLine("execption caught");
+            }
+            Console.ReadKey();
+        }
+    }
+}
