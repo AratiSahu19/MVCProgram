c6ba78b706ff407b3ce093f2d3e84bc6a1cc73b1
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-28 01:23:36 +0530

    find the greater no

---
+++ greaterno/ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("enter 2 numbers ");
+            int n1= Convert.ToInt32(Console.ReadLine());
+            int n2= Convert.ToInt32(Console.ReadLine());
+            if (n1>n2)
+                Console.WriteLine("greatest number is = " + n1 );
+            else
+                Console.WriteLine("greatest number is = " + n2 );
+            Console.ReadKey();
+        }
+    }
+}
