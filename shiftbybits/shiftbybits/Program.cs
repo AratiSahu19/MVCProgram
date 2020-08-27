92fff8d080c5ad5bcd707f9647fa99a3a3fb1069
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-27 12:23:50 +0530

    shiftby bit exmp

---
+++ shiftbybits/shiftbybits/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace shiftbybits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter data");
+            int num = Convert.ToInt32(Console.ReadLine());
+           num <<= 2;
+            int num2 = num;
+            Console.WriteLine("number is" + num2);
+            Console.ReadKey();
+        }
+    }
+}
