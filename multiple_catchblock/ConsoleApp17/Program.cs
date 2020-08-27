6a82ab0cf5dc5269fcca889f832f7f1e1482cb3d
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-28 01:27:01 +0530

    multiple catch block to handle exception

---
+++ multiple_catchblock/ConsoleApp17/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("enter 1st no :");
+                int n1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("enter 2nd no :");
+                int n2 = int.Parse(Console.ReadLine());
+                int k = n1 / n2;
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine("no is not in range");
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine("divide by zero error");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("exception throw");
+            }
+            Console.ReadKey();
+        }
+    }
+}
