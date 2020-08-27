41d2c27c513e66a704880bd11caba533d09d71ef
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-27 12:28:15 +0530

    use of static keyword

---
+++ static_keyword/ConsoleApp41/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp41
+{
+    class Program
+    {
+        static int add()
+        {
+            int a, b;
+            a = b = 7;
+            return a + b;
+
+        }
+        static void Main(string[] args)
+        {
+            int x;
+            x = Program.add();
+
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
