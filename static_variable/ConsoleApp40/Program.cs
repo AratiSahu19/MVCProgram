afde32669d2768f9b466003bafe36b62e70846bf
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-27 12:31:59 +0530

    static variable exmp

---
+++ static_variable/ConsoleApp40/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp40
+{
+    class Program
+    {
+        static int num = 40;
+        public static void exam()
+        {
+            int num = 20;
+            Console.WriteLine(" number " + num);
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("number is "+num);
+            Program.exam();
+            Console.WriteLine("number = " + num);
+            Console.ReadKey();
+
+        }
+    }
+}
