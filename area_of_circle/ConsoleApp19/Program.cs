85cc589d722b8ab77a55a5498565fa56ea650f28
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-28 01:00:02 +0530

    area of circle

---
+++ area_of_circle/ConsoleApp19/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rad;
+           float  area = 0;
+            Console.WriteLine("enter the radius :");
+            rad = Convert.ToInt32(Console.ReadLine());
+            area = 2 * 3.14f * rad;
+            Console.WriteLine("area of circle is " + area);
+            Console.ReadKey();
+        }
+    }
+}
