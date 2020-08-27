5ab34d000b3427ed62ca2f9b27d9af3bc0dee5ca
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-28 01:12:12 +0530

    enum example

---
+++ enum exmp/ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+
+    {
+        enum suit { clubs,spades,diamond,hearts}
+        static void Main(string[] args)
+        {
+            string color;
+            suit tmp = suit.spades ;
+            if (tmp == suit.clubs)
+                color = "Green";
+            else if (tmp == suit.hearts)
+                color = "blue";
+            else if (tmp == suit.diamond)
+                color = "yellow";
+            else
+                color = "black";
+            Console.WriteLine("color is = " + color);
+            Console.ReadKey();
+
+        }
+    }
+}
