d23db466c878609a8c9dc5558543b1d5bcd9d228
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-28 01:01:16 +0530

    armstrong no exmp

---
+++ armstrongno/ConsoleApp24/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, rem, sum = 0;
+
+            Console.Write("Enter a number : ");
+
+            num = int.Parse(Console.ReadLine());
+
+            for (int i = num; i > 0; i = i / 10)
+
+            {
+
+                rem = i % 10;
+
+                sum = sum + rem * rem * rem;
+
+            }
+
+            if (sum == num)
+
+            {
+
+                Console.Write("Entered number is an Armstrong number.");
+
+            }
+
+            else
+
+                Console.Write("Entered number is not an Armstrong number.");
+
+           
+            Console.ReadKey();
+
+        }
+    }
+}
