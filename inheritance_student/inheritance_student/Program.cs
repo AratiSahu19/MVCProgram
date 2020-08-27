8e1fd5ed4d078c1c279db8b5f4c3f39444d2786f
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-27 11:41:48 +0530

    inheritance exmp

---
+++ inheritance_student/inheritance_student/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace inheritance_student
+{
+    class Student
+    {
+        private class s
+        {
+        }
+
+       
+        
+    }
+    class Kia:Student
+    {
+       
+    }
+        
+            
+
+    
+  
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Student s = new Student();
+            Console.WriteLine("this is parent class");
+            Student s1 = new Kia();
+            Console.WriteLine("this is child class");
+            s
+            
+        }
+    }
+}
