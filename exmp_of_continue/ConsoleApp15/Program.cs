4e75badab3fd7db9487e46b44c487c59eb2f0d23
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-28 01:15:51 +0530

    example of continue statement

---
+++ exmp_of_continue/ConsoleApp15/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+           for(int i=0;i<=10;i++)
+            {
+
+                
+               
+                if (i == 5)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+               
+               
+            }
+            Console.ReadKey();
+        }
+    }
+}
