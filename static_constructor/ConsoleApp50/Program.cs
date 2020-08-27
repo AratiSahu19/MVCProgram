59053214dbdac4d5d3d724dc899c6662725f8987
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-27 12:25:51 +0530

    static constructor exmp

---
+++ static_constructor/ConsoleApp50/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;  
+
+namespace ConsoleApp50
+{
+    class simpleclass
+    {
+        public static readonly long baseline;
+
+        static simpleclass()
+        {
+            baseline = DateTime.Now.Ticks;
+            Console.WriteLine("data: " + baseline);
+        }
+        public simpleclass()
+        {
+            Console.WriteLine("hello from public constructor");
+        }
+            
+
+
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            simpleclass s = new simpleclass();
+            
+            Console.ReadKey();
+           
+        }
+    }
+}
