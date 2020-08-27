81a7e8d616d8c122084669f32aaca16ff927d310
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-27 12:12:31 +0530

    calculate salary of employee in hr basis

---
+++ employee_calsalary/practicle_test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace practicle_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int id;
+            float salary, hours, per_month;
+            Console.WriteLine("enter id of employee");
+            id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter per hour salary of employee");
+            salary = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("enter working hours");
+            hours = Convert.ToSingle(Console.ReadLine());
+            per_month = hours * salary;
+            Console.WriteLine("Employee Id=" + id);
+            Console.WriteLine("salary=" + per_month);
+           
+            Console.ReadKey();
+        }
+    }
+}
