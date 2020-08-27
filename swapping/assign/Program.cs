eacc184bec799ac4a2f32b54d7f03ffc3df61748
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-27 12:33:46 +0530

    swapping of two no

---
+++ swapping/assign/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace assign
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=10, b=20;
+            //Console.WriteLine("enter two number");
+            a = a + b;
+             b= a - b;
+            a = b- a;
+            Console.WriteLine("enter swap value");
+            Console.WriteLine("a=" + a);
+            Console.WriteLine("b=" + b);
+           
+            Console.ReadKey();
+
+        }
+    }
+}
