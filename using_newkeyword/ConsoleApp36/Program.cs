01a4d80b49020c44cdac37f345e501de61958f96
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-27 12:41:11 +0530

    using new keyword

---
+++ using_newkeyword/ConsoleApp36/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp36
+{
+    class Program
+    {
+        public void method()
+        {
+            Console.WriteLine("By creating instance of a class");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.method();
+            Console.ReadKey();
+        }
+    }
+}
