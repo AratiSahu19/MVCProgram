abc90bacc340b2c0787891624db4818cd9b47d47
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-27 12:22:05 +0530

    struct exmp

---
+++ struct exmp/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public struct employee
+    {
+        public string frname;
+        public int age;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            employee comemp;
+
+            Console.WriteLine("enter name & age ");
+            comemp.frname = Console.ReadLine();
+            comemp.age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("name= " + comemp.frname);
+            Console.WriteLine("age= " + comemp.age);
+            Console.ReadKey();
+        }
+
+    }
+}
