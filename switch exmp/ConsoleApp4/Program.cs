81f247ff9791919abd645782260e54a080144112
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-28 01:31:00 +0530

    swich case exmp

---
+++ switch exmp/ConsoleApp4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    enum  suit { hearts ,diamond,spades, clubs }
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            string color;
+            suit tmp = suit.clubs ;
+            switch(tmp)
+            {
+                case suit.clubs:
+                
+                    break;
+                case suit.spades:
+                    color = "black";
+                    break;
+                case suit.diamond:
+                case suit.hearts:
+                    color = "green";
+                    break;
+                default:
+                    color = "error";
+                    break;
+            }
+            Console.WriteLine("colour is ="+tmp );
+            Console.ReadKey();
+        }
+    }
+}
