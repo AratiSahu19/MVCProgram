fadd6180b54d703d00e13841e98e5917b6191dc6
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-28 01:08:55 +0530

    break statement exmp

---
+++ exmp_break_statment/ConsoleApp18/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 0;
+            while(i<=10)
+            {
+                Console.WriteLine(i);
+                i++;
+                if(i==5)
+                    {
+                    break;
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
