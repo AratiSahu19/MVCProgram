a488b44a96fa82db48b331170a86534cb27f6cd8
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-27 12:07:41 +0530

    ref examp

---
+++ ref_eexmp/ConsoleApp43/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp43
+{
+    class Program
+    {
+        static float diva ( ref int a,ref float b,ref int c)
+        {
+            int x,z;
+            float y,res;
+            x = a;
+            y = b;
+            z = c;
+            res = x + y + z;
+           
+
+            Console.WriteLine("additin is" +res);
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            
+            int a = 3;
+            int c = 7;
+            float b =1.2f ;
+            diva(ref a, ref b, ref c);
+            Console.ReadKey();
+        }
+    }
+}
