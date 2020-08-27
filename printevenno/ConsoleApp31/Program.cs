d16cab8454cd28aa13ceccd4f78fbca2d46a94ee
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-27 12:05:40 +0530

    print even no exmp

---
+++ printevenno/ConsoleApp31/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("enter numbers ");
+            i = 10;
+            while(i<20)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine("Even no " + i);
+                 
+            }
+            i++;
+            Console.ReadKey();
+
+        }
+    }
+}
