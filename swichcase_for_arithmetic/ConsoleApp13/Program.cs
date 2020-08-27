a40345235f40af196ebf3d8921e20567c2a1b2a4
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-28 01:29:21 +0530

    switch for arithmetic operation

---
+++ swichcase_for_arithmetic/ConsoleApp13/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int res = 0;
+           
+            Console.WriteLine("Enter two numbers :");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the choice :");
+            char x = Convert.ToChar (Console.ReadLine());
+            switch (x)
+            {
+                case '+':
+                    res = num1 + num2;
+                    Console.WriteLine("addition is  " + res);
+                    break;
+                case '-':
+                    res = num1 - num2;
+                    Console.WriteLine("substraction is " + res);
+                    break;
+                case '*':
+                    res = num1 * num2;
+                    Console.WriteLine("multiplication is " + res);
+                    break;
+                case '/':
+                    res = num1 / num2;
+                    Console.WriteLine("division is " + res);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+            
+                    Console.ReadKey();
+
+            
+        }
+    }
+}
