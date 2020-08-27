cd807e38ac28389f0e0e9453b51a80da8b18984a
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-28 00:57:41 +0530

    try catch exmp

---
+++ try_catch/ConsoleApp16/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("enter a number:");
+                int i = int.Parse(Console.ReadLine());
+                Console.WriteLine("number is " + i);
+
+            }
+            catch(OverflowException e )
+            {
+                Console.WriteLine("no is out of range");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("not a valid no ");
+            }
+            Console.WriteLine("code reached till the end ");
+            Console.ReadKey();
+
+
+        }
+    }
+}
+
+
