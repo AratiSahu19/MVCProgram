29260ff0da238bf3dab7043a2ade90bceb2dc974
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-28 01:21:05 +0530

    find the greatest no

---
+++ find_greatestno/ConsoleApp22/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("enter three numbers:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            if (num > num1 && num > num2)
+                Console.WriteLine("greatest is" + num);
+            else if (num1 > num && num1 > num2)
+                Console.WriteLine("greatest is " + num1);
+            else
+                Console.WriteLine("greatest is " + num2);
+            Console.ReadKey();
+        }
+    }
+}
