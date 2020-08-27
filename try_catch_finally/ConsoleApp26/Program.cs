c971910424318c01d68913b29f32c8deb498fa47
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-28 00:58:47 +0530

    try catch finally

---
+++ try_catch_finally/ConsoleApp26/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                int res;
+
+                Console.WriteLine("enter two numbers ");
+                int a = Convert.ToInt32(Console.ReadLine());
+                int b = Convert.ToInt32(Console.ReadLine());
+                res = a / b;
+                Console.WriteLine("number is " + res);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("nothing");
+
+            }
+            finally
+            {
+                Console.WriteLine("no errors");
+
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
