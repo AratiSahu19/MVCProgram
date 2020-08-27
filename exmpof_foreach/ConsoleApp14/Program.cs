67c6a6b86c2225c5f1c319b90bbcf61df38ef7c1
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-28 01:17:34 +0530

    use of foreach loop

---
+++ exmpof_foreach/ConsoleApp14/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] num = new int[] { 2, 4, 6 };
+           
+           
+            foreach (int x in num)
+            { 
+                Console.WriteLine(x);
+            }
+          
+            Console.ReadKey();
+
+        }
+    }
+}
