159f2207e4a55853604886e2621a413377ca93ff
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-27 11:45:17 +0530

    perfect no example

---
+++ perfect_no/ConsoleApp33/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, s = 0;
+            Console.WriteLine("enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (i=1; i <num; i++)
+            {
+                if (num % i == 0)
+                    s = s + i;
+            }
+            if (s == num)
+                Console.WriteLine("perfect number " + s);
+            else
+                Console.WriteLine("not a perfect no " + s);
+            Console.ReadKey();
+
+        }
+    }
+}
