44b65af4db57cbb1e61af8cf2fb51f7ba3966d72
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-28 01:22:36 +0530

    for loop exmp

---
+++ forloop_exmp/ConsoleApp9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int x = 5, i;
+            Console.WriteLine("enter the value of n:");
+            int X = Convert.ToInt32(Console.ReadLine());
+            for (i = 0; i <= X; i++)
+                Console.WriteLine("element is " + X);
+            Console.ReadKey();
+
+
+            
+        }
+    }
+}
