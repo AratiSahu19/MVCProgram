5f9fa4b160c658df471739010626c3eb2d02755f
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-28 01:19:12 +0530

    factorial using dowhile loop

---
+++ factorial_dowhile/ConsoleApp12/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine ("enter a number:");
+            num = Convert.ToInt32(Console.ReadLine());
+            int fact = 1;
+            do
+            {
+                fact = fact * num;
+                num--;
+            } while (num > 0);
+            Console.WriteLine(" factorial is :" + fact);
+            Console.ReadKey();
+
+        }
+    }
+}
