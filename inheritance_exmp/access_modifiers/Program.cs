6f76f8611d16732b378c4720235202345cf2e61c
AratiSahu19 <kumariarati1902@gmail.com>
2020-08-30 18:10:12 +0530

    inheritance exmp

---
+++ inheritance_exmp/access_modifiers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace access_modifiers
+{
+    class BaseClass
+    {
+        public int RollNo;
+    }
+    class ChildClass:BaseClass
+    {
+        public void Getdata(int rno)
+        {
+            RollNo = rno;
+        }
+        public void Showdata()
+        {
+            Console.WriteLine("rno:" + RollNo);
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChildClass cc = new ChildClass();
+            cc.Getdata(22);
+            cc.Showdata();
+            Console.ReadKey();
+        }
+    }
+}
