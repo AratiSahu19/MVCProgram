83edf56d7f2d878baf6715392b890a4f4d8b448e
AratiSahu19 <kumariarati1902@gmail.com>
2020-09-01 11:51:32 +0530

    program added in bulk

---
+++ addition/addition/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addition
+{
+     public class Program
+    {
+        public static int add(int n1, int n2)
+        {
+            return n1+n2;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter two no");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int res = add(n1, n2);
+            Console.WriteLine("addition of two no=" + res);            Console.ReadKey();
+        }
+    }
+}
---
+++ additionform/additionform/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using additionlibrary;
+namespace additionform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Mylibrary ml = new Mylibrary();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textBox1.Text);
+            int num2 = Convert.ToInt32(textBox2.Text);
+            int result = ml.Addition(num1, num2);
+            textBox3.Text = result.ToString();  
+          
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textBox1.Text);
+            int num2 = Convert.ToInt32(textBox2.Text);
+            int result = ml.Subtraction(num1, num2);
+            textBox3.Text = result.ToString();
+
+
+        }
+    }
+}
