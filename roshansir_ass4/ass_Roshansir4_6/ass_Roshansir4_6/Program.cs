8ac33de54bb2613ec501e9e7e68503024ac7dc0d
AratiSahu19 <kumariarati1902@gmail.com>
2020-09-01 12:37:29 +0530

    assignment done

---
+++ roshansir_ass4/ass_Roshansir4_6/ass_Roshansir4_6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ass_Roshansir4_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num,rem=0;
+            Console.WriteLine("Enter the number below 500");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            while (num!=0)
+            {
+                rem = rem+ num % 10;
+                num = num / 10;
+                   
+            }
+            
+            Console.WriteLine("Addition of given number is:"+rem);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ roshansir_ass4/ass_roshansir_4_2/ass_roshansir_4_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass_roshansir_4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("enter the number");
+            num = Convert.ToInt32(Console.ReadLine());
+            int num2= num << 2;
+            Console.WriteLine("num2=" + num2);
+            Console.ReadKey();Z
+
+        }
+    }
+}
