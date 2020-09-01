274b959bc87b539b846837623398426a9ab586db
AratiSahu19 <kumariarati1902@gmail.com>
2020-09-01 11:57:44 +0530

    array examples

---
+++ arraysum/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using arraysum;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        { 
+            int[] arr1 = { 1, 2 ,3 };
+            Program p = new Program();
+            int res = p.arraysum(arr1);
+            int actual = 6;
+            Assert.AreEqual(actual, res);
+
+
+        }
+    }
+
+}
---
+++ arraysum/arraysum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arraysum
+{
+    public class Program
+    {
+        public  int arraysum(int[] arr)
+        {
+            int total = 0;
+            for(int i=0;i<3;i++)
+            {
+                total = total + arr[i];
+            }
+            return total;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            Console.WriteLine("enter element");
+            for (int i = 0; i < 3; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Program p = new Program();
+            int res = p.arraysum(arr);
+            Console.WriteLine("sum of array element=" + res);
+            Console.ReadKey();
+
+        }
+    }
+}
