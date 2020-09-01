3aa8db5a4e47ef7d0725ad56b87d1aabfa270236
AratiSahu19 <kumariarati1902@gmail.com>
2020-09-01 11:54:17 +0530

    commited

---
+++ addittion_2array/addittion_2array/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addittion_2array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[4];
+            int[]b = new int[4];
+            //int result=0,sum=0;
+            int[] result = new int[4];
+            int i ;
+            Console.WriteLine("enter first array element: ");
+            for(i=0;i<=3;i++)
+            {
+              
+                    a[i] = int.Parse(Console.ReadLine());
+               
+            }
+            Console.WriteLine("enter second array element: ");
+            for(i=0;i<=3;i++)
+            {
+              
+                    b[i] = int.Parse(Console.ReadLine());
+               
+            }
+            Console.WriteLine("final array addittion is: ");
+            for (i = 0; i <= 3; i++)
+            {
+
+                result[i] = a[i]+b[i];
+                Console.WriteLine(result[i]);
+
+
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ array/array/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array
+{
+    class Program
+    {
+        //1.program (simple array)
+
+        /*  static void Main(string[] args)
+          {
+              int[] arr = new int[4];
+              int i;
+              Console.WriteLine("enter array element: ");
+              for(i=0;i<=3;i++)
+              {
+                  arr[i] = int.Parse(Console.ReadLine());
+              }
+              Console.WriteLine("printed array elements are: ");
+              for(i=0;i<=3;i++)
+              {
+                  Console.WriteLine(arr[i]);
+              }
+              Console.ReadKey();
+
+          }*/
+
+
+
+
+        //2.program (method without return type and without argument
+        /*static void Main(string[] args)
+        {
+            array();
+            Console.ReadKey();
+        }
+        static void array()
+        {
+            int[] arr = new int[4];
+            int i;
+            Console.WriteLine("enter array element: ");
+            for (i = 0; i <= 3; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("printed array elements are: ");
+            for (i = 0; i <= 3; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }*/
+
+
+
+
+
+
+        //3.program without return type and with argument
+
+        static void Main(string[] args)
+        {
+            int[] arr = new int[4];
+            int i;
+            Console.WriteLine("enter array element: ");
+            for (i = 0; i <= 3; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            array(arr);
+            Console.ReadKey();
+        }
+        static void array(int[] a)
+        {
+            int i;
+            Console.WriteLine("printed array elements are: ");
+            for (i = 0; i <= 3; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+
+        }
+
+
+
+
+
+
+        //4.with return type and without argument
+
+    }
+}
