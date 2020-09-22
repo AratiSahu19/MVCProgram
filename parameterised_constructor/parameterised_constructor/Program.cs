68ce5508fd297c8bb5fbb40464644378a2d02cb4
AratiSahu19 <kumariarati1902@gmail.com>
2020-09-22 10:21:55 +0530

    example of parameterised contructor

---
+++ parameterised_constructor/parameterised_constructor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace parameterised_constructor
+{
+    public class Person
+    {
+        private string first;
+        private string last;
+
+        public Person(string firstname,string lastname)
+        {
+            first = firstname;
+            last = lastname;
+        }
+        public void show()
+        {
+            Console.WriteLine("FirstName=" + first);
+            Console.WriteLine("LastName=" + last);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("amit", "saha");
+            p.show();
+            Console.ReadKey();
+        }
+    }
+}
