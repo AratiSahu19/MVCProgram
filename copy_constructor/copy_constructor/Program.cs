9727b91c253609d2a522c4250228ac2680792df2
AratiSahu19 <kumariarati1902@gmail.com>
2020-09-25 01:16:43 +0530

    example of constructor

---
+++ copy_constructor/copy_constructor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace copy_constructor
+{
+    class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public Person(int age,string name)
+        {
+            Age = age;
+            Name = name;
+        }
+        public Person(Person pv)
+        {
+            Age = pv.Age;
+            Name = pv.Name;
+        }
+        public string Details()
+        {
+            return Name + "  is  " + Age.ToString()+ " years  old.";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person(18, "Jhon");
+            Person p2 = new Person(p);
+            Console.WriteLine("original:" + p.Details());
+            Console.WriteLine("copy:" + p2.Details());
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ default_constructor/default_constructor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace default_constructor
+{
+    class BankAccount
+    {
+        public int actno { get; set; }
+        public BankAccount()
+        {
+            actno = 22;
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount ba = new BankAccount();
+            int no = ba.actno;
+            Console.WriteLine("Account no=" + no);
+            Console.ReadKey();
+        }
+    }
+}
